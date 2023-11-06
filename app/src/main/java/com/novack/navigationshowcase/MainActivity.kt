9f4f6e8033d4c718a62b41b870f77786c0a4e1ca
novack-anggelo <anggelo.novack@gmail.com>
2023-11-06 19:30:00 +0100

    initial commit

---
+++ app/src/main/java/com/novack/navigationshowcase/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.novack.navigationshowcase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.novack.navigationshowcase.ui.theme.NavigationShowcaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationShowcaseTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
